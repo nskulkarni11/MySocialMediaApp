0c85caf2164ed9fa0ac1656bc4f37499c1e1b182
nskulkarni1120 <nskulkarni11@outlook.com>
2023-07-28 22:52:00 +0530

    settings update is working

--- core/views.py
+++ core/views.py
@@ -10,6 +10,38 @@ from .models import Profile
 def index(request):
     return render(request,'index.html')
 
+# @login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        return redirect('settings')
+
+    return render(request,'setting.html',{'user_profile':user_profile})
+
+
+
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -28,12 +60,13 @@ def signup(request):
                 user.save()
 
             # log user and redirect to settings
-
+            user_login = auth.authenticate(username=username,password=password)
+            auth.login(request,user_login)
             # create profile object for new user
             user_model = User.objects.get(username=username)
             new_profile = Profile.objects.create(user= user_model,id_user = user_model.id)
             new_profile.save()
-            return redirect('signin')
+            return redirect('settings')
         else:
             messages.info(request,'Password not matching')
             return redirect('signup')
@@ -58,6 +91,7 @@ def signin(request):
     else:
         return render(request,'signin.html')
     
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
