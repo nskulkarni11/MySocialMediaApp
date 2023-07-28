bcc23d2ac43bd3c10e48df335741dfff4652d905
nskulkarni1120 <nskulkarni11@outlook.com>
2023-07-29 00:09:55 +0530

    post db created and working

--- core/views.py
+++ core/views.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile,Post
 
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    return render(request,'index.html')
+    user_object = User.objects.get(username = request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request,'index.html',{'user_profile' : user_profile})
 
-# @login_required(login_url='signin')
+@login_required(login_url='signin')
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
 
@@ -35,11 +37,24 @@ def settings(request):
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
-        return redirect('settings')
+        return redirect('/')
 
     return render(request,'setting.html',{'user_profile':user_profile})
 
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+        new_post = Post.objects.create(user=user, image = image,caption= caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return redirect('/')
 
+
+    return HttpResponse('<h1>Upload view </h1>')
 
 
 def signup(request):
